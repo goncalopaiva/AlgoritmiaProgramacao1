74ba5142f8a8169eaeeb52c0bb9aea2c63bdc8bb
Goncalo <esa.goncalo@live.com.pt>
2020-11-06 01:54:19 +0000

    First commit

---
+++ cmake-build-debug/CMakeFiles/ProjetoLPIAEDI.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjetoLPIAEDI.dir/main.c.obj"
+  "ProjetoLPIAEDI.exe"
+  "ProjetoLPIAEDI.exe.manifest"
+  "ProjetoLPIAEDI.pdb"
+  "libProjetoLPIAEDI.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ProjetoLPIAEDI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
