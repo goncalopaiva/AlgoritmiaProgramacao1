c3434ef334eb93f957ca407e9c72a25e542a690f
Goncalo <esa.goncalo@live.com.pt>
2020-11-13 10:34:08 +0000

    Adicionada alojamentos.c e alojamentos.h

--- alojamentos.c
+++ alojamentos.c
@@ -3,11 +3,14 @@
 //
 
 #include "alojamentos.h"
+#include "main.c"
 
 int menu_alojamentos() {
 
+    int op =0;
+
     printf("----------------------------------\n");
-    printf("-------- MENU ALOJAMENTOS --------\n")
+    printf("-------- MENU ALOJAMENTOS --------\n");
     printf("----------------------------------\n");
 
     printf("\n\n");
@@ -22,23 +25,55 @@ int menu_alojamentos() {
     printf("\t 2. Adicionar alojamentos\n");
     printf("\t 3. Editar alojamentos\n");
     printf("\t 4. (...)\n");
+    printf("\t 0. Voltar ao menu principal\n");
 
+    do {
+        scanf("%d",&op);
 
+        switch (op)
+        {
+            case 1: ver_alojamentos();
+                break;
+            case 2: adicionar_alojamentos();
+                break;
+            case 3: editar_alojamentos();
+                break;
+            case 0: main();
+
+            default: printf("Opcao Invalida!\n");
+        }
+    } while (op!=0);
+    return 0;
 }
 
 int ver_alojamentos() {
     //Ver todos os alojamentos ou só 1?
     //Se todos -> imprimir todos os alojamentos.
-    //Se só 1 -> introduza o codigo do alojamento
+    //Se só 1 -> introduza o codigo do alojamento.
+
+    linha();
+    printf("----> Lista de todos os alojamentos:\n\n");
+
 
 }
 
 int adicionar_alojamentos() {
+    //Pedir informacao do alojamento (morada, preco, ...)
+    //Código do alojamento -> proximo elemento.
 
+    linha();
+    printf("----> Adicionar novo alojamento:\n\n");
 
 }
 
 int editar_alojamentos() {
+    //Mostrar todos os alojamentos
+    //Pedir codigo do alojamento que quer editar.
+    //Pedir campo que quer editar.
+    //Guardar novas informacao no alojamento.
+
+    linha();
+    printf("----> Editar alojamento:\n\n");
 
 
 }
--- alojamentos.h
+++ alojamentos.h
@@ -6,6 +6,19 @@
 #define PROJETOLPIAEDI_ALOJAMENTOS_H
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define TAM_VECTOR 20
+
+//TODO: editar a estrutura ALOJAMENTO
+
+typedef struct alojamentos {
+    int num_alojamentos;
+    char id[TAM_VECTOR];
+    char nome[TAM_VECTOR];
+    char categoria[TAM_VECTOR];
+} ALOJAMENTO;
 
 /**
  * Funcao que imprime as opçoes do menu de alojamentos.
@@ -17,19 +30,19 @@ int menu_alojamentos();
  * Funcao que permite visualizar os alojamentos.
  * @return
  */
-int ver_alojamentos()
+int ver_alojamentos();
 
 /**
  * Funcao que permite adicionar alojamentos.
  * @return
  */
-int adicionar_alojamentos()
+int adicionar_alojamentos();
 
 /**
  * Funcao que permite editar os alojamentos.
  * @return
  */
-int editar_alojamentos()
+int editar_alojamentos();
 
 
 #endif //PROJETOLPIAEDI_ALOJAMENTOS_H
